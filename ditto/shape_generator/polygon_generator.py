a809b807be3c2135e137ccdff1eb78ea56db0ab5
unknown <50898990+1155107756@users.noreply.github.com>
2022-01-04 16:55:27 +0800

    Update polygon_generator.py

--- ditto/shape_generator/polygon_generator.py
+++ ditto/shape_generator/polygon_generator.py
@@ -19,23 +19,28 @@ class Polygon_generator(Shape_generator):
 
   def generate_one(self, img:np.array):
     H, W = img.shape[1], img.shape[0]
-    r = np.random.uniform(self.radius_range[0] * W, self.radius_range[1] * W)
-    c_w = np.random.randint(r, W - r)
-    c_h = np.random.randint(r, H - r)
-    X, Y = np.ogrid[:W, :H]
-    mask = np.logical_and(np.sqrt((X - c_w)**2 + (Y - c_h)**2) <= self.radius_range[1] * W,
-                          np.sqrt((X - c_w)**2 + (Y - c_h)**2) >= self.radius_range[0] * W - 1)
-    index = np.transpose(np.where(mask))
-    index = np.array(sorted(index, key=lambda x: math.atan2((x[1] - np.mean(index[:, 1])), (x[0] - np.mean(index[:, 0])))))
+    min_radius = self.radius_range[0] * min(W, H)
+    max_radius = self.radius_range[1] * max((W, H))
+    c_w = np.random.randint(max_radius, W - max_radius)
+    c_h = np.random.randint(max_radius, H - max_radius)
     num_points = np.random.randint(self.num_point_range[0], self.num_point_range[1])
-    polygon_index = (np.linspace(0, len(index), num=num_points + 1)).astype(int)
-    if self.irregular:
-      for i in range(len(polygon_index)):
-        polygon_index[i] += np.random.randint(len(index)//num_points)
+    def rotation_matrix(a):
+      return np.array([[np.cos(a), -np.sin(a)], [np.sin(a), np.cos(a)]])
+    polygon = []
+    theta = 0
+    delta = 2 * np.pi / num_points
     if self.random_rotate:
-      polygon_index += np.random.randint(len(index))
-      polygon_index = polygon_index % len(index)
-    polygon = index[polygon_index[:-1]]
+      theta += np.random.randint(0, 360)
+    for i in range(num_points):
+      angle = theta
+      if self.irregular:
+        angle += np.random.randint(0, int(delta)) - 1
+      polygon.append(np.array([c_w, c_h]) + np.dot(rotation_matrix(angle),
+                     [np.random.uniform(min_radius, max_radius), 0]))
+      theta += delta
+
+
+    polygon = np.array(polygon).astype(int)
     mask = np.zeros((W, H))
     cv2.drawContours(mask, [polygon], 0, 1, -1)
 
